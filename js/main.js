980aa988c37256b3ee9dc03d64dff11ba6b94650
plastikSun <marcom2k01@gmail.com>
2023-07-26 17:13:54 +0200

    add: jsnack 4

--- js/main.js
+++ js/main.js
@@ -96,3 +96,36 @@ js3_button.addEventListener("click",function(){
      js3_average.innerHTML += average.toFixed(2)
 
 })
+
+
+// ! JSNACK 4
+// ? correggi il seguente codice.
+
+/*
+--------------------------
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = "";
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+    }
+    console.log(evenNumbers);
+
+    // dovrebbe restituire [1,3,5]	
+--------------------------
+*/
+
+// * SOLUZIONE *
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let evenNumbers = [];
+for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(i)
+        evenNumbers.push(i);
+    }
+}
+console.log(evenNumbers);
+
+// dovrebbe restituire [1,3,5]	
